9011c2cfdcadfee06be1ee5aa9043bfb3aea00fd
Matheus Gaston <69908477+GastonZinhow@users.noreply.github.com>
2025-08-13 20:31:07 -0300

    update: project structure

---
+++ app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import Aside from "./_components/sections/Aside";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <Aside />
+        Home
+      </div>
+    </>
+  )
+}
