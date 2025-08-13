42fdf96e7e44d5a13b32653a987bb51f8344711f
Matheus Gaston <69908477+GastonZinhow@users.noreply.github.com>
2025-08-13 11:13:19 -0300

    feat: navBar

---
+++ app/NavBar.tsx
@@ -0,0 +1,34 @@
+'use client'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+import React from 'react'
+import classnames from 'classnames';
+
+const NavBar = () => {
+    const currentPath = usePathname();
+    const links = [
+        { label: 'Home', href: '/'},
+        { label: 'Resume', href: '/resume'},
+        { label: 'Projects', href: '/projects'},
+        { label: 'Contact', href: '/contact'},
+    ]
+  return (
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+        <Link href="/">Matheus Gaston</Link>
+        <ul className='flex space-x-6'>
+            {links.map(link => 
+            <Link 
+                key={link.href}
+                className={classnames({
+                    'text-zinc-900': link.href === currentPath,
+                    'text-zinc-500': link.href != currentPath,
+                    'hover:text-zinc-800 transition-colors': true
+                })}
+                href={link.href}>{link.label}</Link>)}
+            <li></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
