3518e2a7306a9bdb8c0a84da9a70802dbe2e323f
Mirelly Pego Cordeiro de Alvarenga <mirellypego@gmail.com>
2025-08-14 13:02:49 -0300

    Criação da tela de projects, resume. Mudanças na navbar e na metadata do layout

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,135 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 const ProjectsPage = () => {
-  return (
-    <div>ProjectsPage</div>
-  )
-}
+return (
+  <div className="min-h-screen bg-black text-white p-8">
+    <h1 className="text-5xl font-bold text-center mb-12">PROJECTS</h1>
 
-export default ProjectsPage
+    <section className="flex flex-col lg:flex-row bg-black rounded-lg overflow-hidden shadow-lg mb-12 border border-white rounded-xl">
+      <div className="relative w-full lg:w-1/2 h-64 lg:h-auto">
+        <Image
+          src="/images/Projeto_1.webp"
+          alt="E-commerce Platform"
+          layout="fill"
+          objectFit="cover"
+          className="rounded-t-lg lg:rounded-l-lg lg:rounded-tr-none"
+        />
+      </div>
+      <div className="p-8 w-full lg:w-1/2 flex flex-col justify-between">
+        <div>
+          <h2 className="text-3xl font-bold mb-4 text-white">E-commerce Platform</h2>
+          <p className="text-gray-300 mb-4 leading-relaxed">
+            As a full-stack developer, I transform ideas into complete and intuitive
+            digital solutions. My expertise spans the frontend with React/Angular
+            and the robust backend with Node.js/Python, creating efficient APIs
+            and scalable databases. I'm passionate about clean code,
+            performance, and continuous learning, always pursuing best
+            development practices.
+          </p>
+          <p className="text-gray-300 mb-6 leading-relaxed">
+            I actively collaborate on agile teams, from planning to deployment,
+            proactively solving problems. My adaptability to new technologies
+            ensures the delivery of high-quality projects. I seek opportunities to
+            apply my skills to creating impactful products.
+          </p>
+        </div>
+        <div className="flex flex-wrap gap-3 mt-6">
+          <span className="inline-flex items-center justify-center px-6 py-3 bg-black text-white text-base rounded-md border border-yellow-500
+                             transition-all duration-300 ease-in-out hover:bg-yellow-500 hover:text-black cursor-pointer">Java</span>
+          <span className="inline-flex items-center justify-center px-6 py-3 bg-black text-white text-base rounded-md border border-yellow-500
+                             transition-all duration-300 ease-in-out hover:bg-yellow-500 hover:text-black cursor-pointer">React</span>
+          <span className="inline-flex items-center justify-center px-6 py-3 bg-black text-white text-base rounded-md border border-yellow-500
+                             transition-all duration-300 ease-in-out hover:bg-yellow-500 hover:text-black cursor-pointer">Angular</span>
+          <span className="inline-flex items-center justify-center px-6 py-3 bg-black text-white text-base rounded-md border border-yellow-500
+                             transition-all duration-300 ease-in-out hover:bg-yellow-500 hover:text-black cursor-pointer">SQL</span>
+        </div>
+      </div>
+    </section>
+
+    <section className="flex flex-col lg:flex-row bg-black rounded-lg overflow-hidden shadow-lg mb-12 border border-white rounded-xl">
+      <div className="relative w-full lg:w-1/2 h-64 lg:h-auto">
+        <Image
+          src="/images/Projeto_2.png"
+          alt="Data Analytics Platform for Fintech"
+          layout="fill"
+          objectFit="cover"
+          className="rounded-t-lg lg:rounded-l-lg lg:rounded-tr-none"
+        />
+      </div>
+      <div className="p-8 w-full lg:w-1/2 flex flex-col justify-between">
+        <div>
+          <h2 className="text-3xl font-bold mb-4 text-white">Data Analytics Platform for Fintech</h2>
+          <p className="text-gray-300 mb-4 leading-relaxed">
+            As a software engineer with a data focus, I am a builder of
+            infrastructures that transform large volumes of information into
+            actionable insights. My proficiency lies in creating efficient data
+            pipelines using Python/Spark, developing RESTful APIs for data
+            consumption, and implementing distributed storage and processing
+            solutions (Kafka, Cassandra).
+          </p>
+          <p className="text-gray-300 mb-6 leading-relaxed">
+            I collaborate closely with data scientists and business analysts,
+            designing and implementing solutions that support predictive models
+            and interactive dashboards. My approach is results-oriented, with a
+            focus on continuous delivery and iterative improvement, utilizing agile
+            methodologies.
+          </p>
+        </div>
+        <div className="flex flex-wrap gap-3 mt-6">
+          <span className="inline-flex items-center justify-center px-6 py-3 bg-black text-white text-base rounded-md border border-yellow-500
+                             transition-all duration-300 ease-in-out hover:bg-yellow-500 hover:text-black cursor-pointer">Java</span>
+          <span className="inline-flex items-center justify-center px-6 py-3 bg-black text-white text-base rounded-md border border-yellow-500
+                             transition-all duration-300 ease-in-out hover:bg-yellow-500 hover:text-black cursor-pointer">React</span>
+          <span className="inline-flex items-center justify-center px-6 py-3 bg-black text-white text-base rounded-md border border-yellow-500
+                             transition-all duration-300 ease-in-out hover:bg-yellow-500 hover:text-black cursor-pointer">Angular</span>
+          <span className="inline-flex items-center justify-center px-6 py-3 bg-black text-white text-base rounded-md border border-yellow-500
+                             transition-all duration-300 ease-in-out hover:bg-yellow-500 hover:text-black cursor-pointer">SQL</span>
+        </div>
+      </div>
+    </section>
+
+    <section className="flex flex-col lg:flex-row bg-black rounded-lg overflow-hidden shadow-lg mb-12 border border-white rounded-xl">
+      <div className="relative w-full lg:w-1/2 h-64 lg:h-auto">
+        <Image
+          src="/images/Projeto_3.jpg"
+          alt="Intelligent Hospital Management System"
+          layout="fill"
+          objectFit="cover"
+          className="rounded-t-lg lg:rounded-l-lg lg:rounded-tr-none"
+        />
+      </div>
+      <div className="p-8 w-full lg:w-1/2 flex flex-col justify-between">
+        <div>
+          <h2 className="text-3xl font-bold mb-4 text-white">Intelligent Hospital Management System</h2>
+          <p className="text-gray-300 mb-4 leading-relaxed">
+            As a software engineer, I am committed to developing robust,
+            scalable, and high-quality systems. My expertise spans from data
+            modeling and microservices architecture to the design and
+            implementation of intuitive user interfaces with modern frameworks. I&apos;m
+            apaixonado por performance, clean code, e aplicando SOLID
+            principles to ensure maintainability and scalability.
+          </p>
+          <p className="text-gray-300 mb-6 leading-relaxed">
+            I actively participate in all phases of the software development
+            lifecycle, from requirements gathering and prototyping to automated
+            testing and deployment in cloud environments (AWS/Azure).
+          </p>
+        </div>
+        <div className="flex flex-wrap gap-3 mt-6">
+          <span className="inline-flex items-center justify-center px-6 py-3 bg-black text-white text-base rounded-md border border-yellow-500
+                             transition-all duration-300 ease-in-out hover:bg-yellow-500 hover:text-black cursor-pointer">Java</span>
+          <span className="inline-flex items-center justify-center px-6 py-3 bg-black text-white text-base rounded-md border border-yellow-500
+                             transition-all duration-300 ease-in-out hover:bg-yellow-500 hover:text-black cursor-pointer">React</span>
+          <span className="inline-flex items-center justify-center px-6 py-3 bg-black text-white text-base rounded-md border border-yellow-500
+                             transition-all duration-300 ease-in-out hover:bg-yellow-500 hover:text-black cursor-pointer">Angular</span>
+          <span className="inline-flex items-center justify-center px-6 py-3 bg-black text-white text-base rounded-md border border-yellow-500
+                             transition-all duration-300 ease-in-out hover:bg-yellow-500 hover:text-black cursor-pointer">SQL</span>
+        </div>
+      </div>
+    </section>
+  </div>
+);
+};
+
+export default ProjectsPage;
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,9 +1,65 @@
-import React from 'react'
+'use client';
+
+import React, { useState, useEffect } from 'react';
 
 const ResumePage = () => {
-  return (
-    <div>ResumePage</div>
-  )
-}
+const [visiblePdf, setVisiblePdf] = useState<string | null>(null); 
 
-export default ResumePage
+const pdfPortuguesUrl = '/pdfs/curriculo_portugues.pdf';
+const pdfInglesUrl = '/pdfs/curriculo_ingles.pdf';
+
+const handleButtonClick = (pdfType: 'portugues' | 'ingles') => {
+  setVisiblePdf(pdfType);
+};
+
+return (
+  <div className="min-h-screen bg-black text-white p-8 flex flex-col items-center">
+    <h1 className="text-5xl font-bold text-center mb-8">RESUME</h1>
+
+    <div className="flex space-x-4 mb-8">
+      <button
+        onClick={() => handleButtonClick('portugues')}
+        className={`px-8 py-3 rounded-md border transition-all duration-300 ease-in-out
+                    ${visiblePdf === 'portugues' ? 'bg-white text-black border-white' : 'bg-black text-white border-white hover:bg-gray-800'}`}
+      >
+        PORTUGUÊS
+      </button>
+      <button
+        onClick={() => handleButtonClick('ingles')}
+        className={`px-8 py-3 rounded-md border transition-all duration-300 ease-in-out
+                    ${visiblePdf === 'ingles' ? 'bg-white text-black border-white' : 'bg-black text-white border-white hover:bg-gray-800'}`}
+      >
+        INGLÊS
+      </button>
+    </div>
+
+    <div className="w-full max-w-4xl bg-white rounded-lg shadow-lg overflow-hidden border border-white">
+      {visiblePdf === 'portugues' && (
+        <iframe
+          src={pdfPortuguesUrl}
+          title="Currículo em Português"
+          className="w-full"
+          style={{ height: '80vh' }}
+          frameBorder="0"
+        ></iframe>
+      )}
+      {visiblePdf === 'ingles' && (
+        <iframe
+          src={pdfInglesUrl}
+          title="Resume in English"
+          className="w-full"
+          style={{ height: '80vh' }}
+          frameBorder="0"
+        ></iframe>
+      )}
+      {!visiblePdf && (
+        <div className="flex items-center justify-center h-[80vh] text-gray-700 text-xl">
+          Selecione um idioma para visualizar o currículo.
+        </div>
+      )}
+    </div>
+  </div>
+);
+};
+
+export default ResumePage;
