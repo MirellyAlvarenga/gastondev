531a454a5353b3e615bddc10ef38c1e58c1a9501
Matheus Gaston <69908477+GastonZinhow@users.noreply.github.com>
2025-08-22 09:43:09 -0300

    feat: socials component

--- app/(home)/_components/sections/Aside.tsx
+++ app/(home)/_components/sections/Aside.tsx
@@ -1,3 +1,4 @@
+import Socials from "@/app/_components/socials";
 import { useLanguage } from "@/app/context/LanguageContext";
 import { useTheme } from "next-themes";
 import Link from "next/link";
@@ -42,32 +43,7 @@ const Aside = () => {
           {translations.contact}
         </Link>
       </div>
-      <div className="flex gap-4 my-2 p-2">
-        <a
-          href="https://www.linkedin.com/in/matheus-gaston"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-gray-700 hover:text-blue-500 dark:text-white dark:hover:text-blue-300 text-2xl"
-        >
-          <FaLinkedin />
-        </a>
-        <a
-          href="https://github.com/gastonzinhow"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-gray-700 hover:text-gray-900 dark:text-white dark:hover:text-gray-300 text-2xl"
-        >
-          <FaGithub />
-        </a>
-        <a
-          href="https://www.instagram.com/gastonzinhow"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-gray-700 hover:text-pink-600 dark:text-white dark:hover:text-pink-300 text-2xl"
-        >
-          <FaInstagram />
-        </a>
-      </div>
+      <Socials />
     </aside>
   );
 };
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,6 +3,7 @@
 import React, { useRef, useState, FormEvent } from "react";
 import { sendEmail } from "./email";
 import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import Socials from "../_components/socials";
 
 const ContactPage = () => {
   const formRef = useRef<HTMLFormElement | null>(null);
@@ -74,32 +75,7 @@ const ContactPage = () => {
           </div>
           <div className="mt-8">
             <h2 className="italic text-lg mb-4">SOCIAL MEDIA</h2>
-            <div className="flex gap-4 my-2 p-2">
-              <a
-                href="https://www.linkedin.com/in/matheus-gaston"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-gray-700 hover:text-blue-500 dark:text-white dark:hover:text-blue-300 text-2xl"
-              >
-                <FaLinkedin />
-              </a>
-              <a
-                href="https://github.com/gastonzinhow"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-gray-700 hover:text-gray-900 dark:text-white dark:hover:text-gray-300 text-2xl"
-              >
-                <FaGithub />
-              </a>
-              <a
-                href="https://www.instagram.com/gastonzinhow"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-gray-700 hover:text-pink-600 dark:text-white dark:hover:text-pink-300 text-2xl"
-              >
-                <FaInstagram />
-              </a>
-            </div>
+            <Socials />
           </div>
         </div>
 
