33902f47a144883ce220df6ad586116d00ee5e17
Matheus Gaston <69908477+GastonZinhow@users.noreply.github.com>
2025-08-27 11:15:01 -0300

    fix: z-index

--- app/_components/timeline.tsx
+++ app/_components/timeline.tsx
@@ -52,9 +52,9 @@ export const Timeline = ({ data }: { data: Experience[] }) => {
             key={index}
             className="flex justify-start pt-10 md:pt-28 md:gap-10"
           >
-            <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
+            <div className="sticky flex flex-col md:flex-row z-30 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
               <div className="h-10 absolute left-3 md:left-3 w-10 bg-background border flex items-center justify-center z-40 rounded-full">
-                <div className="h-4 w-4 bg-black border z-40 rounded-full" />
+                <div className="h-4 w-4 bg-black border z-30 rounded-full" />
               </div>
               <h3 className="text-gray-300 hidden md:block md:pl-20 md:text-3xl font-bold text-muted-foreground/50">
                 {item.date}
