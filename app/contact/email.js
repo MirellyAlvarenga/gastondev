bfccea30e732254a5f670eca90101e72270afb1f
Mirelly Pego Cordeiro de Alvarenga <mirellypego@gmail.com>
2025-08-29 12:40:55 -0300

    Atualizações no envio de email

--- app/contact/email.js
+++ app/contact/email.js
@@ -1,10 +1,10 @@
 import emailjs from "emailjs-com";
 
 export const sendEmail = (form) => {
-  return emailjs.sendForm(
-    process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID='service_np5tram',
-    process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID='template_cnclosa',
-    form,
-    process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY='jPZ2jx0wct95Zy1tp'
-  );
+return emailjs.sendForm(
+  process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+  process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+  form,
+  process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+);
 };
